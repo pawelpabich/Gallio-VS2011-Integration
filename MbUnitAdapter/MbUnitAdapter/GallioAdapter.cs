a6024345107dae4be5ca5db993116328005d3a8f
Graham Hay <grahamrhay@gmail.com>
2011-11-16 14:41:12 +0000

    Extracted TestExplorer.

--- MbUnitAdapter/MbUnitAdapter/GallioAdapter.cs
+++ MbUnitAdapter/MbUnitAdapter/GallioAdapter.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Gallio.Common.Messaging;
-using Gallio.Common.Reflection;
+﻿using System.Collections.Generic;
 using Gallio.Loader;
-using Gallio.Model;
-using Gallio.Model.Messages.Exploration;
-using Gallio.Model.Schema;
-using Gallio.Runtime;
-using Gallio.Runtime.Logging;
-using Gallio.Runtime.ProgressMonitoring;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
 
 namespace Microsoft.VisualStudio.TestPlatform.Gallio
 {
@@ -22,11 +12,13 @@ namespace Microsoft.VisualStudio.TestPlatform.Gallio
     public class GallioAdapter : ITestDiscoverer, ITestExecutor
     {
         public const string ExecutorUri = "executor://www.mbunit.com/GallioAdapter";
+
         private readonly TestProperty testIdProperty;
         private readonly TestProperty filePathProperty;
         private readonly TestCaseFactory testCaseFactory;
         private readonly TestResultFactory testResultFactory;
         private readonly TestRunner testRunner;
+        private readonly TestExplorer testExplorer;
 
         public GallioAdapter()
         {
@@ -38,112 +30,14 @@ namespace Microsoft.VisualStudio.TestPlatform.Gallio
             testCaseFactory = new TestCaseFactory(testIdProperty, filePathProperty);
             testResultFactory = new TestResultFactory();
 
+            testExplorer = new TestExplorer(testCaseFactory);
             testRunner = new TestRunner(testCaseFactory, testResultFactory, testIdProperty, filePathProperty);
         }
 
-        #region Test Discovery
-        
-        public void DiscoverTests(IEnumerable<string> sources, ObjectModel.Logging.IMessageLogger logger, ITestCaseDiscoverySink discoverySink)
+        public void DiscoverTests(IEnumerable<string> sources, IMessageLogger logger, ITestCaseDiscoverySink discoverySink)
         {
-            var frameworkLogger = new TestFrameworkLogger(logger);
-
-            try
-            {
-
-                frameworkLogger.Log(LogSeverity.Info, "Gallio starting up");
-
-                var gallioTestscases = GetGallioTestcases(frameworkLogger, sources);
-                if (gallioTestscases != null)
-                {
-                    frameworkLogger.Log(LogSeverity.Info, "Found " + gallioTestscases.Count().ToString());
-                    MapGallioTestCases(gallioTestscases, discoverySink);
-                }
-            }
-            catch (Exception ex)
-            {
-                frameworkLogger.Log(LogSeverity.Error, String.Format("Gallio: Exception discovering tests from {0}", ex));
-            }
+            testExplorer.DiscoverTests(sources, logger, discoverySink);
         }
-
-        private void MapGallioTestCases(IEnumerable<TestData> gallioTestCases, ITestCaseDiscoverySink discoverySink)
-        {
-            foreach (var gallioTestCase in gallioTestCases)
-            {
-                var testCase = testCaseFactory.GetTestCase(gallioTestCase);
-                discoverySink.SendTestCase(testCase);
-
-                if (gallioTestCase.Children.Count > 0)
-                {
-                    MapGallioTestCases(gallioTestCase.AllTests, discoverySink);
-                }
-            }
-        }
-
-        private ICodeElementInfo LoadAssembly(string source, ReflectionOnlyAssemblyLoader loader)
-        {
-            loader.AddHintDirectory(Path.GetDirectoryName(source));
-            return loader.ReflectionPolicy.LoadAssemblyFrom(source);
-        }
-
-        private IEnumerable<TestData> GetGallioTestcases(TestFrameworkLogger frameworkLogger, IEnumerable<string> sources)
-        {
-            try
-            {
-                var testFrameworkManager = RuntimeAccessor.ServiceLocator.Resolve<ITestFrameworkManager>();
-                var loader = new ReflectionOnlyAssemblyLoader();
-
-                IList<ICodeElementInfo> assemblyInfos = sources.Select(source => LoadAssembly(source, loader)).Where(assembly => assembly != null).ToList();
-                
-                var testFrameworkSelector = new TestFrameworkSelector()
-                {
-                    Filter = testFrameworkHandle => testFrameworkHandle.Id != "MSTestAdapter.TestFramework" || testFrameworkHandle.Id != "NUnitAdapter.TestFramework",
-                   
-                    FallbackMode = TestFrameworkFallbackMode.Approximate
-                };
-
-                ITestDriver driver = testFrameworkManager.GetTestDriver(testFrameworkSelector, frameworkLogger);
-                var testExplorationOptions = new TestExplorationOptions();
-
-                var tests = new List<TestData>();
-                MessageConsumer messageConsumer = new MessageConsumer()
-                    .Handle<TestDiscoveredMessage>(message =>
-                                                       {
-                                                           if (message.Test.IsTestCase)
-                                                               tests.Add((message.Test));
-                                                       })
-                    .Handle<AnnotationDiscoveredMessage>(message => message.Annotation.Log(frameworkLogger, true));
-
-                driver.Describe(loader.ReflectionPolicy, assemblyInfos,
-                                testExplorationOptions, messageConsumer, NullProgressMonitor.CreateInstance());
-
-                bool reset = ResetCollectionForExposedTests(tests);
-
-                if (reset)
-                {
-                    tests = null;
-                }
-             
-                return tests;
-            }
-            catch (Exception ex)
-            {
-                frameworkLogger.Log(LogSeverity.Error, "Gallio failed to load tests", ex);
-
-                return null;
-            }
-        }
-
-        /// <summary>
-        /// Reset test collection for NUnit\XUnit so we don't confuse the test runner
-        /// </summary>
-        /// <param name="tests">test collection</param>
-        /// <returns>if NUnit or XUnit found then a valid reset</returns>
-        private static bool ResetCollectionForExposedTests(IEnumerable<TestData> tests)
-        {
-            return tests.Any(test => test.FullName.Contains("NUnitTest") || test.FullName.Contains("XUnitTest"));
-        }
-
-        #endregion
 
         public void Cancel()
         {           
---
+++ MbUnitAdapter/MbUnitAdapter/TestExplorer.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Gallio.Common.Messaging;
+using Gallio.Common.Reflection;
+using Gallio.Model;
+using Gallio.Model.Messages.Exploration;
+using Gallio.Model.Schema;
+using Gallio.Runtime;
+using Gallio.Runtime.Logging;
+using Gallio.Runtime.ProgressMonitoring;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+namespace Microsoft.VisualStudio.TestPlatform.Gallio
+{
+    public class TestExplorer
+    {
+        private readonly ITestCaseFactory testCaseFactory;
+
+        public TestExplorer(ITestCaseFactory testCaseFactory)
+        {
+            this.testCaseFactory = testCaseFactory;
+        }
+
+        public void DiscoverTests(IEnumerable<string> sources, IMessageLogger logger, ITestCaseDiscoverySink discoverySink)
+        {
+            var frameworkLogger = new TestFrameworkLogger(logger);
+
+            try
+            {
+                frameworkLogger.Log(LogSeverity.Info, "Gallio starting up");
+
+                var tests = Explore(sources, frameworkLogger);
+                
+                if (tests == null)
+                    return;
+             
+                frameworkLogger.Log(LogSeverity.Info, "Found " + tests.Count());
+                PublishTests(tests, discoverySink);
+            }
+            catch (Exception ex)
+            {
+                frameworkLogger.Log(LogSeverity.Error, String.Format("Gallio: Exception discovering tests from {0}", ex));
+            }
+        }
+
+        private static IList<TestData> Explore(IEnumerable<string> sources, ILogger frameworkLogger)
+        {
+            try
+            {
+                var testDriver = GetTestDriver(frameworkLogger);
+
+                var tests = new List<TestData>();
+                var messageConsumer = new MessageConsumer()
+                    .Handle<TestDiscoveredMessage>(message =>
+                    {
+                        if (message.Test.IsTestCase)
+                            tests.Add((message.Test));
+                    })
+                    .Handle<AnnotationDiscoveredMessage>(message => message.Annotation.Log(frameworkLogger, true));
+
+                var loader = new ReflectionOnlyAssemblyLoader();
+                var assemblyInfos = sources.Select(source => LoadAssembly(source, loader)).Where(assembly => assembly != null).ToList();
+                var testExplorationOptions = new TestExplorationOptions();
+
+                testDriver.Describe(loader.ReflectionPolicy, assemblyInfos, testExplorationOptions, messageConsumer, NullProgressMonitor.CreateInstance());
+
+                return ResetCollectionForExposedTests(tests) ? null : tests;
+            }
+            catch (Exception ex)
+            {
+                frameworkLogger.Log(LogSeverity.Error, "Gallio failed to load tests", ex);
+                return null;
+            }
+        }
+
+        private static ITestDriver GetTestDriver(ILogger frameworkLogger)
+        {
+            var testFrameworkManager = RuntimeAccessor.ServiceLocator.Resolve<ITestFrameworkManager>();
+
+            var testFrameworkSelector = new TestFrameworkSelector
+            {
+                Filter = testFrameworkHandle => NotMsTestOrNunit(testFrameworkHandle.Id),
+                FallbackMode = TestFrameworkFallbackMode.Approximate
+            };
+
+            var testDriver = testFrameworkManager.GetTestDriver(testFrameworkSelector, frameworkLogger);
+            return testDriver;
+        }
+
+        private static bool NotMsTestOrNunit(string testFrameworkHandleId)
+        {
+            return testFrameworkHandleId != "MSTestAdapter.TestFramework" || testFrameworkHandleId != "NUnitAdapter.TestFramework";
+        }
+
+        private static ICodeElementInfo LoadAssembly(string source, ReflectionOnlyAssemblyLoader loader)
+        {
+            loader.AddHintDirectory(Path.GetDirectoryName(source));
+            return loader.ReflectionPolicy.LoadAssemblyFrom(source);
+        }
+
+        /// <summary>
+        /// Reset test collection for NUnit\XUnit so we don't confuse the test runner
+        /// </summary>
+        /// <param name="tests">test collection</param>
+        /// <returns>if NUnit or XUnit found then a valid reset</returns>
+        private static bool ResetCollectionForExposedTests(IEnumerable<TestData> tests)
+        {
+            return tests.Any(test => test.FullName.Contains("NUnitTest") || test.FullName.Contains("XUnitTest"));
+        }
+
+        private void PublishTests(IEnumerable<TestData> tests, ITestCaseDiscoverySink discoverySink)
+        {
+            foreach (var test in tests)
+            {
+                var testCase = testCaseFactory.GetTestCase(test);
+                discoverySink.SendTestCase(testCase);
+
+                if (test.Children.Count > 0)
+                {
+                    PublishTests(test.AllTests, discoverySink);
+                }
+            }
+        }
+    }
+}
